c9f089aa399296bfd7480365f37b1f821e879488
anvesh <anvesh.tatipelly@anvationlabs.com>
2025-02-16 17:27:03 +0530

    Removed api_const.dart from tracking

--- lib/core/services/api_const.dart
+++
@@ -1,3 +0,0 @@
-const projectUrl = "https://aulbdywqndjypyngzvut.supabase.co";
-const anonPublic =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1bGJkeXdxbmRqeXB5bmd6dnV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ3MzA1MDAsImV4cCI6MjA1MDMwNjUwMH0.-g60skkjveKgVMCgXQrRGTooF3v_-NsQtZJkxct--XA";
